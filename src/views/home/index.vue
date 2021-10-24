39c932fdd45c2784b0c8f510c02a0bb1fc312dfd
Osman Fikret Ceylan <ofcyln@gmail.com>
2021-10-24 16:17:49 +0200

    Initial commit for implementation of the architecture

---
+++ src/views/home/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="./images/logo.png" />
+    <HelloWorld msg="Vue3 Boilerplate" />
+  </div>
+</template>
+
+<script lang="ts">
+import HelloWorld from './components/hello-world.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+}
+</script>
